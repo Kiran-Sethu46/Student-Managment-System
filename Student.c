55ae5164849bbca18c985df771faaa2cfaae7f34
kiran-sethu46 <kiran226242@gmail.com>
2021-05-03 20:17:39 +0530

    base :fire:

---
+++ Student.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+
+#define Max_YR   9999
+#define Min_YR  1000
+#define MAXsize_Username
+#define Maxsize_Password
+#define FILE_NAME "studenrecords.bin"
+
+
+#define MAX_Father_Name 50 
+#define MAX_Student_Name 50 
+#define MAX_student_Address 300
+#define File_Header_Size sizeof(sFileHeader)
+
